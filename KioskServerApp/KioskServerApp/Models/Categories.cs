4ca191080f77eddc595c954bbbdb68a1d4003813
Raul Gheorghiu <124466168+raulg6@users.noreply.github.com>
2024-06-21 14:26:04 +0200

    Add files via upload

---
+++ KioskServerApp/KioskServerApp/Models/Categories.cs
@@ -0,0 +1,32 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System.Collections.Generic;
+
+public class Product
+{
+    [BsonId]
+    [BsonRepresentation(BsonType.ObjectId)]
+    public string Id { get; set; }
+
+    [BsonElement("name")]
+    public string Name { get; set; }
+
+    [BsonElement("price")]
+    public decimal Price { get; set; }
+
+    [BsonElement("img")]
+    public string ImageUrl { get; set; }
+}
+
+public class Category
+{
+    [BsonId]
+    [BsonRepresentation(BsonType.ObjectId)]
+    public string Id { get; set; }
+
+    [BsonElement("name")]
+    public string Name { get; set; }
+
+    [BsonElement("products")]
+    public Dictionary<string, Product> Products { get; set; }
+}
---
+++ KioskServerApp/KioskServerApp/Services/MongoDBService.cs
@@ -0,0 +1,20 @@
+﻿using MongoDB.Driver;
+using Microsoft.Extensions.Configuration;
+
+public class MongoDBService
+{
+    private readonly IMongoClient _client;
+    private readonly IMongoDatabase _database;
+
+    public MongoDBService(IConfiguration config)
+    {
+        string? connectionString = config.GetConnectionString("MongoDbConnection");
+        _client = new MongoClient(connectionString);
+        _database = _client.GetDatabase("kiosk");
+    }
+
+    public IMongoCollection<Category> GetCategoryCollection()
+    {
+        return _database.GetCollection<Category>("category");
+    }
+}
